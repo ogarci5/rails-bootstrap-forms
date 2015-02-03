b4f9ffd3852d3e6336fad9f54f464268f5578ccb
Oliver Garcia <ogarci5@gmail.com>
2015-02-03 04:36:00 -0600

    Class needed to be added as well.

--- lib/bootstrap_form/form_builder.rb
+++ lib/bootstrap_form/form_builder.rb
@@ -179,9 +179,9 @@ module BootstrapForm
 
       options[:class] = ["form-group", options[:class]].compact.join(' ')
       options[:class] << " #{error_class}" if has_error?(name)
-      options[:class] << " #{feedback_class}" if options[:icon]
+      options[:class] << " #{feedback_class}" if options[:icon] || options[:fa_icon]
 
-      content_tag(:div, options.except(:id, :label, :help, :icon, :label_col, :control_col, :layout)) do
+      content_tag(:div, options.except(:id, :label, :help, :icon, :fa_icon, :label_col, :control_col, :layout)) do
         label = generate_label(options[:id], name, options[:label], options[:label_col], options[:layout]) if options[:label]
         control = capture(&block).to_s
         control.concat(generate_help(name, options[:help]).to_s)
